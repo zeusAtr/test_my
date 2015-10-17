7b29499c37b8864bd13908046f9f56965c8a70ab
zeusAtr <zeus.atr@gmail.com>
2015-10-17 23:27:24 +0300

    commit from bash

--- python_project/samples/ex_2.py
+++ python_project/samples/ex_2.py
@@ -5,25 +5,6 @@
 #     l.append(int(item)*3)
 #     print l[int(item)-1]+10
 
-#
-# score = float(raw_input("Enter scode between 0.0 and 1.0:"))
-#
-# while score > 1:
-#     print "ERROR"
-#     exit(1)
-#
-# if score >=0.9:
-#     print "A"
-# elif score >=0.8:
-#     print "B"
-# elif score >=0.7:
-#     print "C"
-# elif score >=0.6:
-#     print "D"
-# else:
-#     print "F"
-
-
 largest = 0
 smallest = 0
 
