8d2a3f7e5d963b61a6c4eaf5453b847bed6dcf01
zeusAtr <zeus.atr@gmail.com>
2015-10-28 00:09:15 +0200

    added install_python.bat

---
+++ python_install.bat
@@ -0,0 +1,21 @@
+@echo off
+
+mkdir C:\Python27
+bitsadmin.exe /transfer "Download python" https://www.python.org/ftp/python/2.7.10/python-2.7.10.msi C:\Python27\python-2.7.10.msi
+msiexec.exe /i C:\Python27\python-2.7.10.msi /QN /L*V "C:\new\msilog.log"
+
+ rem /L*V "C:\Temp\msilog.log"= verbose logging
+ rem /QN = run completely silently
+ rem /i = run install sequence 
+ 
+echo python successfully installed
+ 
+echo set enviroment variable
+setx PATH "C:\Python27;C:\Python27\Lib\site-packages\;C:\Python27\Scripts\"
+
+echo pip install
+start easy_install pip
+echo pip successfully installed
+
+timeout 7
+pause
